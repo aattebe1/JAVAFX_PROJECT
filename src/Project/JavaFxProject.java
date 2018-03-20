4e0ede91c3644bab37c4b989c84c8149188950bd
aattebe1 <37160977+aattebe1@users.noreply.github.com>
2018-03-20 04:35:44 -0400

    Add files via upload

--- src/Project/JavaFxProject.java
+++ src/Project/JavaFxProject.java
@@ -12,6 +12,9 @@ import javafx.application.Application;
 import javafx.animation.Animation;
 import javafx.animation.Transition;
 import javafx.animation.FadeTransition;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 import javafx.stage.Stage;
@@ -19,12 +22,14 @@ import javafx.scene.Scene;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.control.Button;
+import javafx.scene.shape.Sphere;
 import javafx.util.Duration;
 
 public class JavaFxProject extends Application {
@@ -45,8 +50,45 @@ public class JavaFxProject extends Application {
 		/* Object declaration section */
 		String welcomeMsg = "Welcome To\nAustin Atteberry's JavaFx Project";
 		String courseMsg = "CS 1302\nSection 09\nInstructor Carlos A. Cepeda";
-		Text welcomeDisplay = new Text(20, 35, "");
-		Text courseDisplay = new Text(20, 150, "");
+		String authorHeader = "About Me";
+		String authorContent = "Austin Atteberry is a senior majoring\nin Electrical Engineering Technology at\nKennesaw State University. "
+				+ "He enjoys\ncomputer and electronic projects,\nhobbyist level software defined radio,\nand spending time with his amazing\n"
+				+ "wife Emilee. He's also a fan of the NFL\nand IndyCar.\n\nAustin is originally from Indianapolis,\nbut now lives in the "
+				+ "Atlanta area with\nhis wife and their cat. The couple's\nfirst child is due in May.";
+		Text welcomeDisplay = new Text(20, 45, "");
+		Text courseDisplay = new Text(20, 160, "");
+		Text homeDisplay1 = new Text(20, 45, welcomeMsg);
+		Text homeDisplay2 = new Text(20, 160, courseMsg);
+		Text authorText1 = new Text(20, 45, authorHeader);
+		Text authorText2 = new Text(300, 94, authorContent);
+		
+		/* Set text attributes */
+		homeDisplay1.setFont(Font.loadFont(JavaFxProject.class.getResource("/Fonts/arumsansextrabold.ttf").toExternalForm(), 40));
+		homeDisplay1.setFill(Color.WHITE);
+		homeDisplay2.setFont(Font.loadFont(JavaFxProject.class.getResource("/Fonts/arumsansextrabold.ttf").toExternalForm(), 20));
+		homeDisplay2.setFill(Color.WHITE);
+		authorText1.setFont(Font.loadFont(JavaFxProject.class.getResource("/Fonts/arumsansextrabold.ttf").toExternalForm(), 40));
+		authorText1.setFill(Color.FIREBRICK);
+		authorText2.setFont(Font.loadFont(JavaFxProject.class.getResource("/Fonts/arumsansextrabold.ttf").toExternalForm(), 20));
+		authorText2.setFill(Color.FIREBRICK);
+		
+		/* Create sphere animations */
+		Sphere displaySphere0 = CreateSphere(100, 100, 360, Color.LAWNGREEN, Color.BLUE);
+		AnimateSphere(displaySphere0);
+		Sphere displaySphere1 = CreateSphere(100, 100, 360, Color.DARKGREY, Color.AZURE);
+		AnimateSphere(displaySphere1);
+		
+		/* Create Image object */
+		Image austin = new Image("/Pictures/Selfie.jpg");
+		
+		/* Create ImageView object */
+		ImageView author = new ImageView(austin);
+		
+		/* Set image attributes */
+		author.setFitHeight(austin.getHeight() * 0.45);
+		author.setPreserveRatio(true);
+		author.setLayoutX(20);
+		author.setLayoutY(80);
 		
 		/* Create Buttons */
 		Button btHome0 = NewButton("btHome.png", "btHomeActive.png", 20, 500);
@@ -61,6 +103,19 @@ public class JavaFxProject extends Application {
 		Button btGames1 = NewButton("btGames.png", "btGamesActive.png", 412, 500);
 		Button btGames2 = NewButton("btGames.png", "btGamesActive.png", 412, 500);
 		Button btGamesActive = NewActiveButton("btGamesActive.png", 412, 500);
+		Button btVideo = new Button("Watch Video");
+		
+		/* Set button attributes */
+		btVideo.setStyle("-fx-background-color: linear-gradient(#686868 0%, #232723 25%, #373837 75%, #757575 100%),\r\n"
+				+ "linear-gradient(#020b02, #3a3a3a),\r\n"
+				+ "linear-gradient(#9d9e9d 0%, #6b6a6b 20%, #343534 80%, #242424 100%),\r\n"
+				+ "linear-gradient(#8a8a8a 0%, #6b6a6b 20%, #343534 80%, #262626 100%),\r\n"
+				+ "linear-gradient(#777777 0%, #606060 50%, #505250 51%, #2a2b2a 100%);"
+				+ "-fx-effect: dropshadow( three-pass-box , rgba(255,255,255,0.2) , 1, 0.0 , 0 , 1);"
+				+ "-fx-text-fill: white; -fx-font-size: 15px;");
+		btVideo.setPrefSize(120, 30);
+		btVideo.setLayoutX(390);
+		btVideo.setLayoutY(370);
 		
 		/* Create panes */
 		Pane welcomePane = new Pane();
@@ -74,10 +129,17 @@ public class JavaFxProject extends Application {
 		primaryStage.setResizable(false);
 		primaryStage.getIcons().add(new Image("/icon/icon.gif"));
 		
-		welcomePane.getChildren().addAll(welcomeDisplay, courseDisplay, btHome0, btAuthors0, btGames0);
-		homePane.getChildren().addAll(btHomeActive, btAuthors1, btGames1);
-		authorsPane.getChildren().addAll(btHome1, btAuthorsActive, btGames2);
+		/* Add nodes to panes */
+		welcomePane.getChildren().addAll(welcomeDisplay, courseDisplay, displaySphere0, btHome0, btAuthors0, btGames0);
+		homePane.getChildren().addAll(homeDisplay1, homeDisplay2, displaySphere1, btHomeActive, btAuthors1, btGames1);
+		authorsPane.getChildren().addAll(authorText1, authorText2, author, btVideo, btHome1, btAuthorsActive, btGames2);
 		gamesPane.getChildren().addAll(btHome2, btAuthors2, btGamesActive);
+		
+		/* Set background colors */
+		welcomePane.setStyle("-fx-background-color: #f85113;");
+		homePane.setStyle("-fx-background-color: #f85113;");
+		authorsPane.setStyle("-fx-background-color: #ffd700;");
+		gamesPane.setStyle("-fx-background-color: #006400;");
 		
 		/* Create scenes */
 		Scene welcomeView = new Scene(welcomePane);
@@ -85,14 +147,13 @@ public class JavaFxProject extends Application {
 		Scene authorsView = new Scene(authorsPane);
 		Scene gamesView = new Scene(gamesPane);
 		
+		/* Set welcome screen stage */
 		primaryStage.setTitle("JavaFx Project: Welcome");
 		primaryStage.setScene(welcomeView);
 		primaryStage.show();
 		
 		/* Display welcome message */
 		WelcomeMessage(welcomePane, welcomeMsg, welcomeDisplay, courseMsg, courseDisplay);
-		
-		
 		
 		/* Create home button action */
 		btHome0.setOnAction(new EventHandler<ActionEvent>() {
@@ -207,7 +268,7 @@ public class JavaFxProject extends Application {
 	public void WelcomeMessage(Pane welcomePane, String welcomeMsg, Text welcomeDisplay, String courseMsg, Text courseDisplay) {
 		/* Set welcome message font */
 		welcomeDisplay.setFont(Font.loadFont(JavaFxProject.class.getResource("/Fonts/arumsansextrabold.ttf").toExternalForm(), 40));
-		welcomeDisplay.setFill(Color.ORANGERED);
+		welcomeDisplay.setFill(Color.WHITE);
 		
 		/* Declare animation */
 		Animation writeMsg = new Transition() {
@@ -228,7 +289,7 @@ public class JavaFxProject extends Application {
 			public void handle(ActionEvent e) {
 				/* Set course information font */
 				courseDisplay.setFont(Font.loadFont(JavaFxProject.class.getResource("/Fonts/arumsansextrabold.ttf").toExternalForm(), 20));
-				courseDisplay.setFill(Color.ORANGERED);
+				courseDisplay.setFill(Color.WHITE);
 				
 				/* Display course information */
 				courseDisplay.setText(courseMsg);
@@ -327,5 +388,42 @@ public class JavaFxProject extends Application {
 		paneFade.setCycleCount(1);
 		paneFade.setAutoReverse(false);
 		paneFade.play();
+	}
+	
+	
+	/* CreateSphere method
+	 * Description: Creates a sphere with the given coordinates
+	 */
+	public Sphere CreateSphere(double radius, double X, double Y, Color dColor, Color sColor) {
+		/* Create sphere with the given coordinates */
+		Sphere sphere = new Sphere(radius);
+		sphere.setTranslateX(X);
+		sphere.setTranslateY(Y);
+		
+		/* Color the sphere */
+		PhongMaterial sphereColor = new PhongMaterial();
+		sphereColor.setDiffuseColor(dColor);
+		sphereColor.setSpecularColor(sColor);
+		sphere.setMaterial(sphereColor);
+		
+		/* Return sphere */
+		return sphere;
+	}
+	
+	
+	public void AnimateSphere(Sphere sphere) {
+		/* Create timeline and set attributes */
+		Timeline moveBall = new Timeline();
+		moveBall.setCycleCount(Timeline.INDEFINITE);
+		moveBall.setAutoReverse(true);
+		
+		/* Add keyframes */
+		moveBall.getKeyFrames().addAll(
+				new KeyFrame(Duration.millis(2000),
+						new KeyValue(sphere.translateXProperty(), 535), new KeyValue(sphere.translateYProperty(), 360))
+				);
+		
+		/* Play animation */
+		moveBall.play();
 	}
 }
